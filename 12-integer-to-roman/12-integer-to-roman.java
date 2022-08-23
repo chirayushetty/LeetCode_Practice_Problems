f6c846c3299edb18030e1d451209c7716f877a2e
chirayushetty <73223171+chirayushetty@users.noreply.github.com>
2022-08-23 10:07:32 +0530

    Time: 26 ms (13.12%), Space: 46.6 MB (27.17%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,17 +1,25 @@
+// Amazon interview:
+
 class Solution {
     public String intToRoman(int num) {
-        int[] numint = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] numstr = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+//         int[] numint = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+//         String[] numstr = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
-        StringBuilder sb = new StringBuilder();
+//         StringBuilder sb = new StringBuilder();
         
-        for(int i=0;i<numint.length;i++){
-            while(num >= numint[i]){
-                sb.append(numstr[i]);
-                num -= numint[i];
-            }
-        }
+//         for(int i=0;i<numint.length;i++){
+//             while(num >= numint[i]){
+//                 sb.append(numstr[i]);
+//                 num -= numint[i];
+//             }
+//         }
         
-        return sb.toString();
+//         return sb.toString();
+        
+        String M[] = {"", "M", "MM", "MMM"};
+    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
     }
 }
