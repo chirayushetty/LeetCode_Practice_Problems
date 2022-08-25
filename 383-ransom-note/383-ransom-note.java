888bc7b743817ee343059145eb58b21da31913d6
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-08-25 11:17:33 +0530

    Time: 19 ms (47.37%), Space: 52.7 MB (20.40%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,21 +1,35 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if(ransomNote.length() > magazine.length()) return false;
-        if(ransomNote.isEmpty() && magazine.isEmpty()) return true;
-        
-        int[] alphacounter = new int[26];
+        HashMap<Character,Integer> map = new HashMap<>();
         
         for(char c : magazine.toCharArray()){
-            alphacounter[c - 'a']++;
+            map.put(c , map.getOrDefault(c,0)+1);
         }
         
         for(char c : ransomNote.toCharArray()){
-            if(alphacounter[c - 'a']==0) return false;
-            alphacounter[c - 'a']--;
+            if(!map.containsKey(c) || map.get(c)==0) return false;
+            
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)-1);
+            }
         }
         
         return true;
+        
+//         if(ransomNote.length() > magazine.length()) return false;
+        
+//         int[] alphacounter = new int[26];
+        
+//         for(char c : magazine.toCharArray()){
+//             alphacounter[c - 'a']++;
+//         }
+        
+//         for(char c : ransomNote.toCharArray()){
+//             if(alphacounter[c - 'a']==0) return false;
+//             alphacounter[c - 'a']--;
+//         }
+        
+//         return true;
     }
 }
 
-// ransomNote length is larger than that of magazine, just return false
