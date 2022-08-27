62b8ba07c872cd3568e2530505764168c11f3f96
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-08-27 23:23:00 +0530

    Time: 3 ms (69.40%), Space: 42.2 MB (88.62%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -4,18 +4,18 @@ class Solution {
     public String addBinary(String a, String b) {
        StringBuilder sb = new StringBuilder();
        
-       int i = a.length() - 1;
-       int j = b.length() - 1;
-       int carry = 0;
+       int i = a.length() - 1,j = b.length() - 1,carry = 0;
+       // int j = b.length() - 1;
+       // int carry = 0;
         
        while(i>=0 || j>=0){
            int sum = carry;
-           if(i>=0) sum += a.charAt(i) - '0';
-           if(j>=0) sum += b.charAt(j) - '0';
+           if(i>=0) sum += a.charAt(i--) - '0';
+           if(j>=0) sum += b.charAt(j--) - '0';
            sb.append(sum%2); // if sum==2 or sum==0 append 0 cause 1+1=0 in this case as                                  this is base 2 (just like 1+9 is 0 if adding ints in columns
            carry = sum / 2;  //if sum==2 we have a carry, else no carry 1/2 rounds down to 0                              in integer arithematic
-           i--;
-           j--;
+           // i--;
+           // j--;
        }
        if(carry!=0) sb.append(carry);
        return sb.reverse().toString();
