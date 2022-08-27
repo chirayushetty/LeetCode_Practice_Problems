b9ac2f12d0d8647278ab58221326bb731d678dfd
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-08-27 23:33:02 +0530

    Time: 3 ms (84.69%), Space: 43.5 MB (51.06%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -1,19 +1,21 @@
+// Refer leetcode 67 before this.
+
 class Solution {
     public String addStrings(String num1, String num2) {
         StringBuilder sb = new StringBuilder();
         
-        int i = num1.length()-1;
-        int j = num2.length()-1;
-        int carry = 0;
+        int i = num1.length()-1,j = num2.length()-1,carry = 0;
+        // int j = num2.length()-1;
+        // int carry = 0;
         
         while(i>=0 || j>=0){
             int sum = carry;
-            if(i>=0) sum += num1.charAt(i) - '0';
-            if(j>=0) sum += num2.charAt(j) - '0';
+            if(i>=0) sum += num1.charAt(i--) - '0';
+            if(j>=0) sum += num2.charAt(j--) - '0';
             sb.append(sum%10);
             carry = sum / 10;
-            i--;
-            j--;
+            // i--;
+            // j--;
         }
         
         if(carry!=0) sb.append(carry);
