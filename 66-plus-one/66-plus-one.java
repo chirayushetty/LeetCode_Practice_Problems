a01cd58611cd10efa1f676bf9ea11f7d9af499c9
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-08-27 10:58:31 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (56.31%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,46 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-//         for(int i=digits.length-1 ; i>=0 ;i--){
-//             if(digits[i] < 9){
-//                 digits[i]++;
-//                 return digits;
-//             }
-//             digits[i] = 0;
-//         }
-        
-//         int[] new_digits = new int[digits.length+1];
-//         new_digits[0] = 1;
-    
-//         return new_digits;
-        
-         int n = digits.length;
-    for(int i=n-1; i>=0; i--) {
-        if(digits[i] < 9) {
-            digits[i]++;
-            return digits;
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
         
-        digits[i] = 0;
-    }
-    
-    int[] newNumber = new int [n+1];
-    newNumber[0] = 1;
-    
-    return newNumber;
+        int[] new_digits = new int[digits.length+1];
+        new_digits[0] = 1;
+        
+        return new_digits;
     }
 }
-    
-//     int n = digits.length;
-//     for(int i=n-1; i>=0; i--) {
-//         if(digits[i] < 9) {
-//             digits[i]++;
-//             return digits;
-//         }
-        
-//         digits[i] = 0;
-//     }
-    
-//     int[] newNumber = new int [n+1];
-//     newNumber[0] = 1;
-    
-//     return newNumber;
