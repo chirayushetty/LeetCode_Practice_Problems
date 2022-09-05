ec4fa6618412967ec180183f26eb0a12dcd2b607
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-09-06 00:05:52 +0530

    Time: 3 ms (28.91%), Space: 40.7 MB (69.91%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.java
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.java
@@ -1,21 +1,22 @@
-class Solution
-{
-    public int countEven(int num)
-    {
+class Solution {
+    public int countEven(int num) {
         int count = 0;
-        for(int i = 1; i <= num; i++)
-            if(sumDig(i))
+        for(int i=1;i<=num;i++){
+            if(sumEven(i)){
                 count++;
+            }
+        }
+        
         return count;
     }
-    static boolean sumDig(int n)
-    {
+    
+    static boolean sumEven(int n){
         int sum = 0;
-        while(n > 0)
-        {
+        while(n>0){
             sum += n % 10;
             n /= 10;
         }
-		return (sum&1) == 0 ? true : false;
+        
+        return (sum&1)==0 ? true : false;
     }
 }
