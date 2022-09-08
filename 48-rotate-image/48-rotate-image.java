cf3b1b85f563768ec444634a7871a3df79e86dec
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-09-08 15:36:44 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (36.84%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,27 @@
+// https://www.javatpoint.com/rotate-matrix-by-90-degrees-in-java
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        
+        for(int i=0;i<len;i++){
+            for(int j=i;j<len;j++){
+                if(i!=j){
+                int temp=0;
+                temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+                }
+            }
+        }
+
+        for(int i=0;i<len;i++){
+            for(int j=0;j<len/2;j++){
+                int temp=0;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[i][len-1-j];
+                matrix[i][len-1-j]=temp;
+            }
+        }
+    }
+}
