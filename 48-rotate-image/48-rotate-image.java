58718a145100da49238c5d90c2092f0f6bf598e1
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-09-08 23:34:18 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (19.98%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -4,17 +4,19 @@ class Solution {
     public void rotate(int[][] matrix) {
         int len = matrix.length;
         
+        //Transpose:
         for(int i=0;i<len;i++){
             for(int j=i;j<len;j++){
                 if(i!=j){
-                int temp=0;
-                temp = matrix[i][j];
+                //int temp=0;
+                int temp = matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
                 }
             }
         }
-
+        
+        // Swap elements of first column with last column:
         for(int i=0;i<len;i++){
             for(int j=0;j<len/2;j++){
                 int temp=0;
