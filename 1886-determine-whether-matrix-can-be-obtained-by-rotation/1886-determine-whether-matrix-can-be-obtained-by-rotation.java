3ea9ab3029d7c207956fd74aff5e1decd14799df
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-09-08 15:32:20 +0530

    Time: 1 ms (86.41%), Space: 40.7 MB (91.48%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -4,36 +4,71 @@
 
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        if(mat==target) return true;
-        int n = mat.length;
-        
-        int[][] res1 = new int[n][n];
-        for(int i=0;i<n;i++){         //90 degree
-            for(int j=0;j<n;j++){
-                res1[i][j] = mat[n-1-j][i];
-            }
-        }
-        
-        int[][] res2 = new int[n][n];
-        for(int i=0;i<n;i++){        //180 degree
-            for(int j=0;j<n;j++){
-                res2[i][j] = res1[n-1-j][i];
-            }
-        }
-        
-        int[][] res3 = new int[n][n];
-        for(int i=0;i<n;i++){       //270 degree
-            for(int j=0;j<n;j++){
-                res3[i][j] = res2[n-1-j][i];
-            }
-        }
-        
-        //compare to 90,180,270 and itself
-        // Arrays.deepEquals() use for matrix
-        if(Arrays.deepEquals(target,res1) || Arrays.deepEquals(target,res2) ||                          Arrays.deepEquals(target,res3) || Arrays.deepEquals(target,mat)){
+    
+    int len = mat.length;
+   
+   //90 degree * 4 = 360degree, we can rotate 90 degree & check whether it matches the target 
+    for(int i=0; i<4; i++){
+        if(Arrays.deepEquals(mat,target))
             return true;
-        }
-        
-        return false;
+        rotate90Degrees(mat,len);
     }
+    return false;
 }
+
+public void rotate90Degrees(int[][]mat, int len){
+    
+    for(int i=0;i<len;i++){
+        for(int j=i;j<len;j++){
+            if(i!=j){
+            int temp=0;
+            temp = mat[i][j];
+            mat[i][j]=mat[j][i];
+            mat[j][i]=temp;
+            }
+        }
+    }
+    
+    for(int i=0;i<len;i++){
+        for(int j=0;j<len/2;j++){
+            int temp=0;
+            temp=mat[i][j];
+            mat[i][j]=mat[i][len-1-j];
+            mat[i][len-1-j]=temp;
+        }
+    }
+    
+}
+}
+
+//         if(mat==target) return true;
+//         int n = mat.length;
+        
+//         int[][] res1 = new int[n][n];
+//         for(int i=0;i<n;i++){         //90 degree
+//             for(int j=0;j<n;j++){
+//                 res1[i][j] = mat[n-1-j][i];
+//             }
+//         }
+        
+//         int[][] res2 = new int[n][n];
+//         for(int i=0;i<n;i++){        //180 degree
+//             for(int j=0;j<n;j++){
+//                 res2[i][j] = res1[n-1-j][i];
+//             }
+//         }
+        
+//         int[][] res3 = new int[n][n];
+//         for(int i=0;i<n;i++){       //270 degree
+//             for(int j=0;j<n;j++){
+//                 res3[i][j] = res2[n-1-j][i];
+//             }
+//         }
+        
+//         //compare to 90,180,270 and itself
+//         // Arrays.deepEquals() use for matrix
+//         if(Arrays.deepEquals(target,res1) || Arrays.deepEquals(target,res2) ||                          Arrays.deepEquals(target,res3) || Arrays.deepEquals(target,mat)){
+//             return true;
+//         }
+        
+//         return false;
