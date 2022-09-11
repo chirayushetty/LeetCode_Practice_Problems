1b35cb6a97d50f633ecf29451f4cbfb921c84608
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-09-12 00:11:25 +0530

    Time: 37 ms (12.15%), Space: 65.8 MB (28.96%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -4,44 +4,24 @@
 
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-//         Arrays.sort(arr);
-//         List<List<Integer>> ans =new ArrayList<>();
-        
-//         int min = Integer.MAX_VALUE;
-//         for(int i=0;i<arr.length-1;i++){
-//             min = Math.min(min,arr[i+1]-arr[i]);
-//         }
-        
-//         for(int i=0;i<arr.length-1;i++){
-//             List<Integer> list = new ArrayList<>();
-//             if(arr[i+1]-arr[i]==min){
-//                 list.add(arr[i]);
-//                 list.add(arr[i+1]);
-//                 ans.add(list);
-//             }
-//         }
-        
-//         return ans;
-        
-        List<List<Integer>> res = new ArrayList();
-        //sort elements
         Arrays.sort(arr);
-        //init our min difference value
+        List<List<Integer>> ans =new ArrayList<>();
+        
         int min = Integer.MAX_VALUE;
-        //start looping over array to find real min element. Each time we found smaller difference
-        //we reset resulting list and start building it from scratch. If we found pair with the same
-        //difference as min - add it to the resulting list
-        for (int i = 0; i < arr.length - 1; i++) {
-            int diff = arr[i + 1] - arr[i];
-            if (diff < min) {
-                min = diff;
-                res.clear();
-                res.add(Arrays.asList(arr[i], arr[i + 1]));
-            } else if (diff == min) {
-                res.add(Arrays.asList(arr[i], arr[i + 1]));
+        for(int i=0;i<arr.length-1;i++){
+            min = Math.min(min,arr[i+1]-arr[i]);
+        }
+        
+        for(int i=0;i<arr.length-1;i++){
+            List<Integer> list = new ArrayList<>();
+            if(arr[i+1]-arr[i]==min){
+                list.add(arr[i]);
+                list.add(arr[i+1]);
+                ans.add(list);
             }
         }
-        return res;
+        
+        return ans;
     }
 }
 
