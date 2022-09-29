11200837952372ef63ce19280d98e9532eb7c8aa
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-09-29 14:30:57 +0530

    Time: 8 ms (92.67%), Space: 77.1 MB (69.39%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,37 @@
+// Google interview (most asked):
+// Longest contiguous subarray of only 2 elements
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        
+// https://youtu.be/s_zu2dOkq80
+        int last_fruit = -1;
+        int second_last_fruit = -1;
+        int last_fruit_count = 0;
+        int max = 0;
+        int current_max = 0;
+        
+        for(int fruit : fruits){
+            if(fruit==last_fruit || fruit==second_last_fruit){
+                current_max += 1;
+            }else{
+                current_max = last_fruit_count + 1;
+            }
+            
+            if(fruit==last_fruit){
+                last_fruit_count += 1;
+            }else{
+                last_fruit_count = 1;
+            }
+            
+            if(fruit!=last_fruit){
+                second_last_fruit = last_fruit;
+                last_fruit = fruit;
+            }
+            
+            max = Math.max(current_max,max);
+        }
+        
+        return max;
+    }
+}
