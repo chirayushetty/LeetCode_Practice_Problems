fac485504c576381d113fd5b001b0b17e5a54766
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-10-01 10:27:37 +0530

    Time: 4 ms (99.08%), Space: 50.6 MB (85.59%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,51 +1,32 @@
 class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
-//         int[] ans = new int[nums.length];
+        int[] ans = new int[nums.length];
         
-//         int evenSum = 0;
-//         for(int num : nums){
-//             if(num%2==0){
-//                 evenSum += num;   
-//             }
-//         }
-        
-//         for(int i=0;i<queries.length;i++){
-//             int index = queries[i][1];
-//             int val = queries[i][0];
-            
-//             if(nums[index]%2==0){
-//                 evenSum -= nums[index];
-//             }
-            
-//             nums[index] += val;
-            
-//             if(nums[index]%2==0){
-//                 evenSum += nums[index];
-//             }
-            
-//             ans[i] = evenSum;
-//         }
-        
-//         return ans;
-        
-        
-        int S = 0;
-        for (int x: nums)
-            if (x % 2 == 0)
-                S += x;
-
-        int[] ans = new int[queries.length];
-
-        for (int i = 0; i < queries.length; ++i) {
-            int val = queries[i][0], index = queries[i][1];
-            if (nums[index] % 2 == 0) S -= nums[index];
-            nums[index] += val;
-            if (nums[index] % 2 == 0) S += nums[index];
-            ans[i] = S;
+        int evenSum = 0;
+        for(int num : nums){
+            if(num%2==0){
+                evenSum += num;
+            }
         }
-
+        
+        for(int i=0;i<queries.length;i++){
+            int index = queries[i][1];
+            int val = queries[i][0];
+            
+            if(nums[index]%2==0){
+                evenSum -= nums[index];
+            }
+            
+            nums[index] += val;
+            
+            if(nums[index]%2==0){
+                evenSum += nums[index];
+            }
+            
+            ans[i] = evenSum;
+        }
+        
         return ans;
-    
     }
 }
 
