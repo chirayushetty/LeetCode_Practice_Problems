8799ff956c3fc793f6be597e9755cf7c55702344
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-10-10 00:09:15 +0530

    Time: 15 ms (64.83%), Space: 44.4 MB (67.09%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = Integer.toString(x);
-        int len = s.length();
-        for(int i=0;i<len/2;i++){
-            if(s.charAt(i) != s.charAt(len-i-1)){
-                return false;
-            }
-        }
+//         String s = Integer.toString(x);
+//         int len = s.length();
         
-        return true;
+//         for(int i=0;i<len/2;i++){
+//             if(s.charAt(i) != s.charAt(len-i-1)){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        int sum = 0, target = x;
+        while(x>0){
+            int temp = x % 10;
+            x /= 10;
+            sum = sum * 10  + temp;
+        }
+        return sum == target;
     }
 }
