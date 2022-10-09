5e75c81a34e538f73e8a3b7ae631518a211cb69f
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-10-09 23:59:23 +0530

    Time: 17 ms (38.55%), Space: 45.6 MB (20.05%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+        int len = s.length();
+        for(int i=0;i<len/2;i++){
+            if(s.charAt(i) != s.charAt(len-i-1)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
