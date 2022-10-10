aff19f21a598f818015e8733d70078bd62ff51dc
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-10-10 14:54:28 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (31.17%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,9 +1,11 @@
 class Solution {
     public String breakPalindrome(String palindrome) {
+        // How to detect if there is impossible to perform the replacement? Only when the length = 1.
         if(palindrome.length() == 1){
             return "";
         }
         
+        //Change the first non 'a' character to 'a'.
         char[] pal = palindrome.toCharArray();
         int len = palindrome.length();
         
@@ -14,8 +16,8 @@ class Solution {
             }
         }
         
+        //if we reach here, there are ONLY 'a' in palindrome string, so we should change the last character to a b
         pal[len-1] = 'b';
-        
         return String.valueOf(pal);
     }
 }
