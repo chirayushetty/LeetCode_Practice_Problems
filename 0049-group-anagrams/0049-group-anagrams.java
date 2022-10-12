a2851be8a3e7e35c6a08a4f8358558f06bcf7fbb
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-10-13 00:04:44 +0530

    Time: 13 ms (71.73%), Space: 55.4 MB (71.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,16 +1,34 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs==null || strs.length==0)  return new ArrayList<>();
+//         if(strs==null || strs.length==0)  return new ArrayList<>();
         
-        Map<String,List<String>> map = new HashMap<>();
+//         Map<String,List<String>> map = new HashMap<>();
         
-        for(String s : strs){
-            char[] ca = s.toCharArray();
-            Arrays.sort(ca);
-            String keyStr = String.valueOf(ca);
-            if(!map.containsKey(keyStr))   map.put(keyStr,new ArrayList<>());
-            map.get(keyStr).add(s);
+//         for(String s : strs){
+//             char[] ca = s.toCharArray();
+//             Arrays.sort(ca);
+//             String keyStr = String.valueOf(ca);
+//             if(!map.containsKey(keyStr))   map.put(keyStr,new ArrayList<>());
+//             map.get(keyStr).add(s);
+//         }
+//         return new ArrayList<>(map.values());
+        
+        //       Initialization of Hashmap Key is type of String and Value is type of List<String
+        HashMap<String, List<String> > map = new HashMap<>(); 
+        
+        
+        for(String s : strs){ // Taking one by one String from strs array
+            char[] chars = s.toCharArray(); //Converting String to Char Array
+            Arrays.sort(chars); // Sorting The Char Array by using inbuilt Sorting Method
+            String key = String.valueOf(chars); //Converting Sorted Char array to String. It will act as a Key for our hashmap
+            if(map.containsKey(key)){
+                map.get(key).add(s); //If key exist then add the String or append the string to the end of the list
+            }else{
+                map.put(key, new ArrayList<>()); // If key not present then initialize the ArrayList 
+                map.get(key).add(s); // and add the Perticular String to that key.
+            } 
         }
+        
         return new ArrayList<>(map.values());
     }
 }
