6013d8ccf49a0567d4e2bd037a1eef4fcb4030c4
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-10-16 00:19:05 +0530

    Time: 27 ms (46.13%), Space: 41.9 MB (68.20%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.java
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.java
@@ -1,16 +1,17 @@
 class Solution {
     public int countQuadruplets(int[] nums) {
-    int res = 0, n = nums.length;
-    for (int i = 0; i < n; ++i)
-        for (int j = i + 1; j < n; ++j)
-            for (int k = j + 1; k < n; ++k)
-                for (int l = k + 1; l < n; ++l)
-                     if(nums[i] + nums[j] + nums[k] == nums[l]){
-                         res++;
-                     }
-    return res;    
+       int res = 0, n = nums.length;
+        for (int i = 0; i < n-3; i++)
+            for (int j = i + 1; j < n-2; j++)
+                for (int k = j + 1; k < n-1; k++)
+                    for (int l = k + 1; l < n; l++)
+                         if(nums[i] + nums[j] + nums[k] == nums[l]){
+                             res++;
+                         }
+        return res;   
+    }
 }
-}
+
 
 //TC : O(N4)
 // int res = 0, n = nums.length;
@@ -21,4 +22,4 @@ class Solution {
 //                      if(nums[i] + nums[j] + nums[k] == nums[l]){
 //                          res++;
 //                      }
-//     return res;    
+//     return res;   
