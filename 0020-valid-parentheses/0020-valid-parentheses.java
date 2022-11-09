19b4e8859d937e7280562efa7f4a39edb026ac85
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-11-10 00:38:54 +0530

    Time: 3 ms (79.32%), Space: 42.3 MB (19.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,41 @@
 class Solution {
     public boolean isValid(String s) {
-//         while (s.indexOf("{}") !=-1 || s.indexOf("[]") !=-1 || s.indexOf("()") !=-1 ){
-//         s=s.replace("()","");
-//         s=s.replace("[]","");
-//         s=s.replace("{}","");
+// Using string: 
         
-//     }
-//     return s.isEmpty();
+        // if(s.length()%2==1){
+        //     return false;
+        // }
+        // int check = 0;
+        // while(s.length()>0){
+        //     check = s.length();
+        //     s=s.replace("()", "");
+        //     s=s.replace("[]", "");
+        //     s=s.replace("{}", "");
+        //     if(check == s.length()){
+        //         return false;
+        //     }
+        // }
+        // return true;
         
-        if(s.length()%2==1){
-            return false;
-        }
-        int check = 0;
-        while(s.length()>0){
-            check = s.length();
-            s=s.replace("()", "");
-            s=s.replace("[]", "");
-            s=s.replace("{}", "");
-            if(check == s.length()){
-                return false;
+// Using stack:
+        
+        Stack<Character> st = new Stack<>();
+        for(char a : s.toCharArray()){
+            if(a=='(' || a=='{' || a=='['){
+                st.push(a);
+            }else{
+                if(st.isEmpty())  return false;
+                
+                char ch = st.pop();
+                if((ch=='(' && a==')') || (ch=='{' && a=='}') || (ch=='[' && a==']')){
+                    continue;
+                }else {
+                    return false;
+                }
             }
         }
-        return true;
+        
+        return st.isEmpty();
+        
     }
 }
