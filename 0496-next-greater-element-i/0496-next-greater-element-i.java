4938b45b1b4732f0340415f925935de13bfce46f
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-11-11 00:04:26 +0530

    Time: 3 ms (97.05%), Space: 42.6 MB (85.80%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,59 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] idx = new int[nums1.length];
+        // Brute force:
+        // TC: O(n * m), SC: O(n)
+//         int[] idx = new int[nums1.length];
         
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    idx[i]=j;
+//         for(int i=0;i<nums1.length;i++){
+//             for(int j=0;j<nums2.length;j++){
+//                 if(nums1[i]==nums2[j]){
+//                     idx[i]=j;
+//                 }
+//             }
+//         }
+        
+//         int[] ans = new int[nums1.length];
+        
+//         for(int i=0;i<nums1.length;i++){
+//             for(int j=idx[i]+1;j<nums2.length;j++){
+//                 if(nums2[j]>nums2[idx[i]]){
+//                     ans[i] = nums2[j];
+//                     break;
+//                 }
+//             }
+//              if (ans[i] == 0) ans[i] = -1;
+//         }
+        
+//         return ans;
+        
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        int n = nums2.length;
+        
+        for(int i=n-1;i>=0;i--){
+            while(st.isEmpty()==false && st.peek()<=nums2[i]){
+                st.pop();
+            }
+            
+            if(i<n){
+                if(st.isEmpty()==false){
+                    hm.put(nums2[i],st.peek());
+                }else{
+                    hm.put(nums2[i],-1);
                 }
             }
+            
+            st.push(nums2[i]);
         }
         
-        int[] ans = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            for(int j=idx[i]+1;j<nums2.length;j++){
-                if(nums2[j]>nums2[idx[i]]){
-                    ans[i] = nums2[j];
-                    break;
-                }
+         int[] nge = new int[nums1.length];
+        
+        for(int j=0;j<nums1.length;j++){
+            if(hm.containsKey(nums1[j])){
+                nge[j]=hm.get(nums1[j]);
             }
-             if (ans[i] == 0) ans[i] = -1;
         }
+        return nge;
         
-        return ans;
     }
 }
