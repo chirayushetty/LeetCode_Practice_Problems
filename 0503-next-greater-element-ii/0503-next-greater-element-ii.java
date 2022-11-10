7655be60c9382e7e79550d02edc560195c901c18
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-11-10 11:23:58 +0530

    Time: 186 ms (5.94%), Space: 55 MB (37.36%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,27 +1,48 @@
 // https://youtu.be/Du881K7Jtk8
+// i%n is done for circular array.
+// TC & SC : O(n)
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        Stack<Integer> st = new Stack<>();
-        int n = nums.length;
-        int[] nge = new int[n];
+//         Stack<Integer> st = new Stack<>();
+//         int n = nums.length;
+//         int[] nge = new int[n];
         
         
-        for(int i=2*n-1;i>=0;i--){
-            while(st.isEmpty()==false && st.peek()<=nums[i%n]){
-                st.pop();
-            }
+//         for(int i=2*n-1;i>=0;i--){
+//             while(st.isEmpty()==false && st.peek()<=nums[i%n]){
+//                 st.pop();
+//             }
             
-            if(i<n){
-                if(st.isEmpty()==false){
-                    nge[i]=st.peek();
-                }else{
-                    nge[i] = -1;
+//             if(i<n){
+//                 if(st.isEmpty()==false){
+//                     nge[i]=st.peek();
+//                 }else{
+//                     nge[i] = -1;
+//                 }
+//             }
+            
+//             st.push(nums[i%n]);
+//         }
+        
+//         return nge;
+        
+// Brute Force
+// O(n2)
+        
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = -1;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[(i + j) % n] > nums[i]) {
+                    ans[i] = nums[(i + j) % n];
+                    break;
                 }
             }
-            
-            st.push(nums[i%n]);
         }
-        
-        return nge;
+
+        return ans;
     }
 }
