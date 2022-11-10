89d7fed66a5b251bf77859698500866bb2888747
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-11-10 11:24:16 +0530

    Time: 213 ms (5.02%), Space: 54.7 MB (59.66%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -35,7 +35,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             ans[i] = -1;
 
-            for (int j = 0; j < n; j++) {
+            for (int j = 1; j < n; j++) {
                 if (nums[(i + j) % n] > nums[i]) {
                     ans[i] = nums[(i + j) % n];
                     break;
