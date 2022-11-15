ffd0675a1a8e61cd659fa7ab69e2abb929996832
Chirayu Shetty <73223171+chirayushetty@users.noreply.github.com>
2022-11-15 11:39:29 +0530

    Time: 7 ms (72.81%), Space: 42.3 MB (85.54%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,18 +1,37 @@
 class Solution {
     public String removeOuterParentheses(String s) {
+// O(N) Time, O(N) space
+//         StringBuilder sb = new StringBuilder();
+        
+//         int count= 0;
+//         for(char c : s.toCharArray()){
+//             if(c=='('){
+//                 if(count != 0) sb.append(c);
+//                 count++;
+//             }else{
+//                 count--;
+//                 if(count != 0)  sb.append(c);
+//             }
+//         }
+        
+//         return sb.toString();
+        
+        Stack<Character> st = new Stack<>();
         StringBuilder sb = new StringBuilder();
         
-        int count= 0;
         for(char c : s.toCharArray()){
-            if(c=='('){
-                if(count != 0) sb.append(c);
-                count++;
-            }else{
-                count--;
-                if(count != 0)  sb.append(c);
-            }
+           if(c=='('){
+               if(st.size()>=1){
+                   sb.append(c);
+               }
+               st.push(c);
+           }else{
+               if(st.size()>1){
+                   sb.append(c);
+               }
+               st.pop();
+           }
         }
-        
         return sb.toString();
     }
 }
